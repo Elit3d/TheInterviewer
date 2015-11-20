2d94d5085195a2b0344f2827fb78d2df754f607a
WASDMagician <WASDMagician@gmail.com>
2015-11-20 21:25:45 +0000

    Re-worked associates and added basic script layout

---
+++ Assets/Update_Current_Associate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Update_Current_Associate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Update_Image()
+    {
+
+    }
+
+    public void Update_Text()
+    {
+
+    }
+}
---
+++ Assets/update_stats.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class update_stats : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Update_Name()
+	{
+
+	}
+
+	public void Update_Image()
+	{
+
+	}
+
+	public void Update_Skill()
+	{
+
+	}
+
+	public void Update_Level()
+	{
+
+	}
+
+	public void Update_Morale()
+	{
+
+	}
+}
