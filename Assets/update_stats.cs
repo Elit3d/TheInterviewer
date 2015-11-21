a0b0c4418d9a195e6691e39002d41ab4950056a8
WASDMagician <WASDMagician@gmail.com>
2015-11-21 10:15:54 +0000

    Associate_Stats get their own references

--- Assets/update_stats.cs
+++ Assets/update_stats.cs
@@ -7,12 +7,16 @@ public class update_stats : MonoBehaviour {
 	public Image current_image;
 	public Text name_text;
 	public Text skill_text;
-	public Text Level_Text;
+	public Text level_text;
 	public Text morale_text;
 
 	// Use this for initialization
 	void Start () {
-	
+        current_image = transform.GetChild(0).GetComponent<Image>();
+        name_text = transform.GetChild(1).transform.GetChild(0).GetComponent<Text>();
+        skill_text = transform.GetChild(1).transform.GetChild(1).GetComponent<Text>();
+        level_text = transform.GetChild(1).transform.GetChild(2).GetComponent<Text>();
+        morale_text = transform.GetChild(1).transform.GetChild(3).GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
