a52224bce23d113efa29039c4e1a6b1a76e523a8
Kyle Thomas <kyle.j.t@live.co.uk>
2015-11-22 15:03:51 +0000

    Begin_Mission stuff - needs fixing with the text

--- Assets/Begin_Mission.cs
+++ Assets/Begin_Mission.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Begin_Mission : MonoBehaviour {
@@ -9,6 +10,7 @@ public class Begin_Mission : MonoBehaviour {
 	int thief_points;
 	int spy_points;
 	int scout_points;
+    private Text m_text;
 
 	// Use this for initialization
 	void Start () {
@@ -16,40 +18,53 @@ public class Begin_Mission : MonoBehaviour {
 		total_points = 0;
 	}
 
+    void OnLevelWasLoaded()
+    {
+        m_text = GameObject.FindWithTag("FinalText").GetComponent<Text>();
+    }
+
 	public void Start_The_Mission()
 	{
 	   for (int i = 0; i < g_controller.associates.Count; i++)
 	   {
 		   if(g_controller.associates[i].skill == "Lockpicking")
 		   {
-
+               lock_pick_points += g_controller.associates[i].level;
 		   }
 		   if(g_controller.associates[i].skill == "Spy")
 		   {
-
+               spy_points += g_controller.associates[i].level;
 		   }
-		   if(g_controller.associates[i].skill == "")
+		   if(g_controller.associates[i].skill == "Thief")
 		   {
-
+               thief_points += g_controller.associates[i].level;
 		   }
-		   if(g_controller.associates[i].skill == "")
-		   total_points += g_controller.associates[i].level + g_controller.associates[i].morale;
+		   if(g_controller.associates[i].skill == "Scout")
+           {
+               scout_points += g_controller.associates[i].level;
+           }
+		   total_points += g_controller.associates[i].morale;
+           
 	   }
+       total_points += lock_pick_points + spy_points + thief_points + scout_points;
 
+       Application.LoadLevel("mission_scene");
 	   Check_Total_Points();
 		//if points  is correct, we want it to be successful
-	   Application.LoadLevel("mission_scene");
+	   
 	}
 
 	void Check_Total_Points()
-	{   
+	{
+        print(total_points);
+
 		if (total_points >= 10)
 		{
-			Debug.Log("worked");
+            m_text.text = "AKDMKAKDAMDKMAD";
 		}
 		else
 		{
-			Debug.Log("failed");
+            m_text.text = "AKDMKAKDAMDKMAD";
 		}
 	}
 }
