a7b2d06bfd58ce8661c78dc442f3bd6b8eb107c1
WASDMagician <WASDMagician@gmail.com>
2015-11-22 14:29:05 +0000

    stuff

--- Assets/Begin_Mission.cs
+++ Assets/Begin_Mission.cs
@@ -5,6 +5,10 @@ public class Begin_Mission : MonoBehaviour {
 
 	private game_controller g_controller;
 	int total_points;
+	int lock_pick_points;
+	int thief_points;
+	int spy_points;
+	int scout_points;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +20,19 @@ public class Begin_Mission : MonoBehaviour {
 	{
 	   for (int i = 0; i < g_controller.associates.Count; i++)
 	   {
+		   if(g_controller.associates[i].skill == "Lockpicking")
+		   {
+
+		   }
+		   if(g_controller.associates[i].skill == "Spy")
+		   {
+
+		   }
+		   if(g_controller.associates[i].skill == "")
+		   {
+
+		   }
+		   if(g_controller.associates[i].skill == "")
 		   total_points += g_controller.associates[i].level + g_controller.associates[i].morale;
 	   }
 
